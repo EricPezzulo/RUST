34d573a70902f92e5b92e797e0f512a2bb55bce9
Eric Pezzulo <eric.pezzulo@gmail.com>
2022-12-03 19:06:43 -0500

    just learning the syntax

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand::Rng;
+
+
+fn main () {
+    
+    println!("Guess a number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    let mut num_guess = String::new();
+    
+    io::stdin()
+        .read_line(&mut num_guess)
+        .expect("Failed to read number");
+
+
+     println!("Your guess: {num_guess}");
+    
+    let num:i32 = num_guess.trim().parse().unwrap();
+     println!("{}",num); 
+  if num > secret_number {
+        println!("Your guess is too high, the number was {secret_number}");
+    }
+    if num < secret_number {
+        println!("Your guess is too low, the number was {secret_number}");
+    }
+   if num == secret_number {
+       println!("Your guess is CORRECT")
+   }
+    
+
+}
