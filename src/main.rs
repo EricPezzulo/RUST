16ed442e04738568bbb172cbd1bc7a1fb9ea541d
Eric Pezzulo <eric.pezzulo@gmail.com>
2022-12-03 19:11:22 -0500

    refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use std::io;
 use rand::Rng;
 
-
 fn main () {
-    
+   
     println!("Guess a number");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
     let mut num_guess = String::new();
     
     io::stdin()
         .read_line(&mut num_guess)
         .expect("Failed to read number");
-
 
      println!("Your guess: {num_guess}");
     
